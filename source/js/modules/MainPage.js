c0fe48d24cb0123ae104d8583f519d348587e1a0
Aleksandr <akapacuk@gmail.com>
2020-12-18 07:12:27 +0400

    feat: add load data in worldmap

--- source/js/modules/MainPage.js
+++ source/js/modules/MainPage.js
@@ -1,21 +1,46 @@
+import {WorldMap} from './WorldMap';
+
 export class MainPage {
   constructor(rootElement) {
     this.rootElement = rootElement;
+    this.headerElement = null;
+    this.footerElement = null;
+    this.contentElement = null;
+    this.tableContainer = null;
+    this.listContainer = null;
+    this.mapContainer = null;
+    this.graphicContainer = null;
   }
 
   init() {
-
+    this.renderHeader();
+    this.renderContent();
+    this.renderFooter();
   }
 
   renderHeader() {
-
+    this.headerElement = document.createElement('header');
+    this.headerElement.classList.add('header');
+    this.headerElement.textContent = 'Coronavirus Dashboard';
   }
 
   renderContent() {
+    this.contentElement = document.createElement('main');
+    this.contentElement.classList.add('main');
 
+    this.tableContainer = document.createElement('div');
+    this.listContainer = document.createElement('div');
+    this.mapContainer = document.createElement('div');
+    this.graphicContainer = document.createElement('div');
+    
+    this.contentElement.append(this.tableContainer, this.listContainer, this.mapContainer, this.graphicContainer);
+
+    const worldMap = new WorldMap(this.mapContainer);
+    worldMap.init();
   }
 
   renderFooter() {
-
+    this.footerElement = document.createElement('footer');
+    this.footerElement.classList.add('footer');
   }
 }
