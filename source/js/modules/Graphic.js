250055bfbafabf1e736142d68b9da3a1c9936cd4
Grigorovich Tatyana <gtm003@gmail.com>
2020-12-20 13:11:56 +0300

    feat: add containerSwitcher and containerOptions

--- source/js/modules/Graphic.js
+++ source/js/modules/Graphic.js
@@ -1,23 +1,78 @@
 const URL = {
   SUMMARY: 'https://api.covid19api.com/summary',
-  TIME_LINE: 'https://covid19-api.org/api/timeline',
-  GOOGLE_CHART: 'https://www.gstatic.com/charts/loader.js',
-  GLOBAL_TOTAL: 'https://api.covid19api.com/world?from=2020-04-14T00:00:00Z&to=2020-12-19T00:00:00Z',
+  TIME_LINE: 'https://covid19-api.org/api/timeline',                                                      //unstable access
+  COUNTRY_TOTAL: 'https://api.covid19api.com/dayone/country/countryName/status/statusName',
+  GOOGLE_CHART: 'https://www.gstatic.com/charts/loader.js',                                               
+  GLOBAL_TOTAL: 'https://api.covid19api.com/world?from=2020-04-14T00:00:00Z&to=2020-12-19T00:00:00Z',     //incorrect data 
 };
 const DATE_START = new Date(2020, 3, 14, 0, 0, 0, 0);
 
 export class Graphic {
-  constructor(rootElement, data, country) {
+  constructor(rootElement) {
     this.rootElement = rootElement;
-    this.data = data;
-    this.country = country;  
+    this.data = null;
+    this.country = null;
+    this.indexOfCountry = null;
+    this.dataAttributeBottomSwitcher = 'Confirmed';
+    this.dataAttributeHeaderSwitcher = 'All period';  
   }
 
   init() {
+    this.renderContent();
     this.addLibraryGoogleChart().then(() => this.drawGraphic('Global'));
   }
 
   renderContent() {
+    const containerGraphic = document.createElement('div');
+
+    const containerSwitcher = document.createElement('div');
+    const switcherLeft = document.createElement('div');
+    const switcherText = document.createElement('div');
+    const switcherRight = document.createElement('div');
+
+    const containerChart = document.createElement('div');
+    const chart = document.createElement('div');
+    containerChart.append(chart);
+
+    const containerOptions = document.createElement('div');
+
+    for (let i = 0; i < 3; i += 1) {
+      const option = document.createElement('div');
+      switch (i) {
+        case 0:
+          option.textContent = 'Confirmed';
+          break;
+        case 1:
+          option.textContent = 'Dead';
+          break;
+        case 2:
+          option.textContent = 'Recovered';
+          break;
+      }
+      option.classList.add('options__item');
+      containerOptions.append(option);
+    }
+
+    containerGraphic.classList.add('container-graphic');
+
+    containerSwitcher.classList.add('container-switcher');
+    switcherLeft.classList.add('container-switcher__left');
+    switcherText.classList.add('container-switcher__title');
+    switcherRight.classList.add('container-switcher__right');
+    switcherLeft.textContent = '<';
+    switcherRight.textContent = '>';
+    switcherText.textContent = 'All period';
+
+    containerChart.classList.add('container-chart');
+    chart.classList.add('chart');
+
+    containerOptions.classList.add('options');
+
+    containerSwitcher.append(switcherLeft, switcherText, switcherRight);
+
+    containerGraphic.append(containerSwitcher, containerChart, containerOptions);
+    this.rootElement.append(containerGraphic);
+    //this.getData();
 
   }
     
@@ -93,14 +148,20 @@ export class Graphic {
           };
 
           let chart = new google.visualization.LineChart(
-          document.querySelector(".graphic_container")
+          document.querySelector('.chart')
           );
 
           chart.draw(data, options);
         } else {
-          document.querySelector(".graphic_container").innerHTML = "No data";
+          document.querySelector('.chart').innerHTML = "No data";
         }
       });   
     }
+  }
+
+  clearChart() {
+    const containerChart = document.querySelector('.container-chart');
+    const chart = document.querySelectorAll('.chart');
+    containerChart.removeChild(chart);
   }
 }
