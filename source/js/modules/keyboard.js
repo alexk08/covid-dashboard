a6bfbfc5b45b45359fb1edf838ffcb5a903bec36
Alexey Yanvarev <yanvarev_2015@mail.ru>
2020-12-21 19:16:46 +0400

    feat: change api and some data for list

--- source/js/modules/keyboard.js
+++ source/js/modules/keyboard.js
@@ -206,7 +206,6 @@ export const Keyboard = {
 
           keyElement.addEventListener('click', () => {
             textarea.focus();
-            console.log(document.querySelector('.use-keyboard-input'));
             if (this.properties.shift) {
               if (this.properties.language === 'en') {
                 const changedSymbolIndexEn = ['\`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', '[', ']', ';', '\'', '\\', ',', '.', '/'].indexOf(key);
